a443254250a88c6de4b2a3d0940ca9514a3e61cf
thallesenrick <thallesenrick39@gmail.com>
2023-06-20 15:08:03 -0300

    atu

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'roxo-fundo': "url('/src/roxofundo.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
